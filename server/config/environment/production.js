b717e059f1209e51bbbc2e8b5a4d327cb6014e52
RohitB97 <rohit.bokam97@gmail.com>
2016-12-10 22:39:32 +0530

    added files

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,6 +3,10 @@
 // Production specific configuration
 // =================================
 
+var conf = JSON.parse(process.env.APP_CONFIG);
+
+var mongoPassword = 'y7uy7uy7u';
+
 module.exports = {
         // Server IP
         ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
@@ -12,7 +16,7 @@ module.exports = {
 
         // MongoDB connection options
         mongo: {
-                uri: process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/erpchemclave'
+                uri: process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || "mongodb://" + conf.mongo.user + ":" + mongoPassword + "@" + conf.mongo.hostString
         }
 };
 //# sourceMappingURL=production.js.map
